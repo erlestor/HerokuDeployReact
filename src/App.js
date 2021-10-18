546cd00dc0274fee2305df4b5a8b7c36519a2d41
Erlend Storsve <erlestor@ntnu.no>
2021-10-18 21:03:12 +0200

    updated app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-      <h1>Det funke for faen!</h1>
+      <h1 style={{ fontSize: "150px" }}>HÆÆÆÆÆÆÆÆ?</h1>
     </div>
   )
 }
